007ec2246a108936a080ae4ae6d98000e330a002
wa1ead <waliddraa2@gmail.com>
2024-09-10 17:58:04 +0100

    step 45

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -3,9 +3,14 @@ const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+  rows.push(character);
+  character.repeat(i);
 }
 
 let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
 
 console.log(result);
