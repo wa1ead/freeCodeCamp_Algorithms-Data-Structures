1cd75e2cf14ad45d6854d7c0e737b08fbb7748f6
wa1ead <waliddraa2@gmail.com>
2024-09-11 10:41:50 +0100

    step 70

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -3,12 +3,11 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return character.repeat(rowNumber);
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
 }
 
-
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
+  rows.push(padRow(i + 1, count));
 }
 
 let result = ""
