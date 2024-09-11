b43acba3e0e3e461ae347ac56c7e1572b7bc7431
wa1ead <waliddraa2@gmail.com>
2024-09-11 10:02:09 +0100

    step 50

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -2,9 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow() {}
+const call = padRow();
+console.log(call);
+
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character);
-  character.repeat(i);
+  rows.push(character.repeat(i + 1));
 }
 
 let result = "";
