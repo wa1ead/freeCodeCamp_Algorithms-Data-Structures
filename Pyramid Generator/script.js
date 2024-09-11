00a9493869379c4b837b9c98dd6fabdd8e2f6340
wa1ead <waliddraa2@gmail.com>
2024-09-11 10:18:53 +0100

    step 60

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -3,22 +3,21 @@ const count = 8;
 const rows = [];
 
 function padRow(name) {
-  return name;
+  const test = "Testing";
+  console.log('This works!');
+  return test;
+  console.log('This works!')
 }
-const call = padRow("WALID");
+
+const call = padRow("CamperChan");
 console.log(call);
 
-function addTwoNumbers(a, b) {
-  return a + b;
-}
-const sum = addTwoNumbers(5, 10);
-console.log(sum);
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+  rows.push(character.repeat(i + 1))
 }
 
-let result = "";
+let result = ""
 
 for (const row of rows) {
   result = result + "\n" + row;
