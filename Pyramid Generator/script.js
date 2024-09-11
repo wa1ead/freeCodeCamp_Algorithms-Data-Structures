587a2eb2842ceb845a416ac8ed63a80de0b49891
wa1ead <waliddraa2@gmail.com>
2024-09-11 12:34:50 +0100

    FINISHED

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -1,30 +1,25 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
-let inverted = true;
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
 
-// TODO: use a different type of loop
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
   } else {
-
+    rows.push(padRow(i, count));
   }
 }
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
-
-let result = ""
+let result = "";
 
 for (const row of rows) {
   result = result + "\n" + row;
