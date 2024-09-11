0122f4a06f5d04e3afa7c5045cbb954c3c9c9845
wa1ead <waliddraa2@gmail.com>
2024-09-11 10:53:34 +0100

    step 80

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -6,9 +6,13 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-
-for (let i = 1; i <= count; i++) {
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
+}*/
+
+if ("false") {
+  console.log("Condition is true");
 }
 
 let result = ""
