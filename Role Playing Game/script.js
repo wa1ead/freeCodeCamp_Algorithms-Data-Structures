55f8726fab378c70d2bb94e7aaf343edf5692377
wa1ead <waliddraa2@gmail.com>
2024-09-12 11:36:20 +0100

    step 35

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -15,4 +15,17 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth")
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+//initialize buttons
+function goStore() {
+  console.log("Going to store.");
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
