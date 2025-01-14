fddf38b7c914f5958986af1a82c3e214e633c492
wa1ead <waliddraa2@gmail.com>
2025-01-14 11:23:33 +0100

    step 70

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -42,20 +42,21 @@ button3.onclick = fightDragon;
 
 function update(location) {
   button1.innerText = location["button text"][0];
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
 function goTown() {
   update(locations[0]);
 }
 
-function goStore() {}
+function goStore() {
+  update(locations[1]);
+}
 
 function goCave() {
   console.log("Going to cave.");
@@ -68,3 +69,7 @@ function fightDragon() {
 function buyHealth() {}
 
 function buyWeapon() {}
+
+function fightSlime() {}
+
+function fightBeast() {}
