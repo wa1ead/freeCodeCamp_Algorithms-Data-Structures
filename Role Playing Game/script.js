bdd09d22bc7c352328e1f639b2bc6ffa4ae66b20
wa1ead <waliddraa2@gmail.com>
2025-01-14 11:29:33 +0100

    step 75

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -33,6 +33,12 @@ const locations = [
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
   },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
 ];
 
 // initialize buttons
@@ -59,14 +65,17 @@ function goStore() {
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  gold -= 10;
+  health += 10;
+}
 
 function buyWeapon() {}
 
