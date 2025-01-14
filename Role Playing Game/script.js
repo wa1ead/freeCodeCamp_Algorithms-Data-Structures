e62db913702640aaca0b20795294d89730dfa698
wa1ead <waliddraa2@gmail.com>
2025-01-14 11:50:07 +0100

    step 80

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -16,6 +16,8 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [];
+
 const locations = [
   {
     name: "town square",
@@ -73,8 +75,14 @@ function fightDragon() {
 }
 
 function buyHealth() {
-  gold -= 10;
-  health += 10;
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
 }
 
 function buyWeapon() {}
