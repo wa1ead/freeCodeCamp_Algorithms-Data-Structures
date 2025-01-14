0f64ed0115906b17159207d81fa12dc54d2d7642
wa1ead <waliddraa2@gmail.com>
2025-01-14 10:59:56 +0100

    step 50

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -16,11 +16,19 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [];
+const cat = {
+  name: "Whiskers",
+  "Number of legs": 4,
+};
+console.log(cat);
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function update(location) {}
 
 function goTown() {
   button1.innerText = "Go to store";
