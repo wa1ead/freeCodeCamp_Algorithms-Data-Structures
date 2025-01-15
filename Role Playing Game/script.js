8ec4a0b6faa4c46a9263f46a8b6affeb3932455c
wa1ead <waliddraa2@gmail.com>
2025-01-15 13:35:41 +0100

    step 95

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -96,6 +96,10 @@ function buyWeapon() {
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeaponIndex].name;
     text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
