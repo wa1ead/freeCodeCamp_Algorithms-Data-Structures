8024cfe436a2e952cc2dee0be5595a3368ec3901
wa1ead <waliddraa2@gmail.com>
2025-01-15 13:27:15 +0100

    step 85

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -16,8 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const weapons = [];
-
+const weapons = [
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 },
+];
 const locations = [
   {
     name: "town square",
@@ -85,7 +89,12 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeaponIndex++;
+  }
+}
 
 function fightSlime() {}
 
