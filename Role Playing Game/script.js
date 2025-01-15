1c72ac596b26659d7fd34e5d5db2b5e977be7b74
wa1ead <waliddraa2@gmail.com>
2025-01-15 13:31:57 +0100

    step 90

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -93,6 +93,9 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeaponIndex].name;
+    text.innerText = "You now have a " + newWeapon + ".";
   }
 }
 
