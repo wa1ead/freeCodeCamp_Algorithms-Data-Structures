fcc9f3b93bf25320341de2b3adec4eec7437655b
wa1ead <waliddraa2@gmail.com>
2025-01-15 13:41:16 +0100

    step 100

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -90,16 +90,20 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeaponIndex++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeaponIndex].name;
-    text.innerText = "You now have a " + newWeapon + ".";
-    inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: " + inventory;
+  if (currentWeaponIndex < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
   } else {
-    text.innerText = "You do not have enough gold to buy a weapon.";
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 
