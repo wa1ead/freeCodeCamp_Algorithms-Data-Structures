3563ddb861793c938f8a0798aecf1550db05db5b
wa1ead <waliddraa2@gmail.com>
2025-01-22 13:52:42 +0100

    step 135

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -68,6 +68,16 @@ const locations = [
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
   },
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to town square",
+      "Go to town square",
+      "Go to town square",
+    ],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
 ];
 
 // initialize buttons
@@ -185,6 +195,10 @@ function dodge() {
 
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
 }
 
 function lose() {}
