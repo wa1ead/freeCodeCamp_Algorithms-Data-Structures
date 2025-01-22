3a4b988c2783491c322ec11b06c13d01c3597699
wa1ead <waliddraa2@gmail.com>
2025-01-22 13:40:00 +0100

    step 125

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -165,6 +165,13 @@ function goFight() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -=
+    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function dodge() {}
