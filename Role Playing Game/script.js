9a971bcfb99a1faa68d1a4ace69c23ea0834ed87
wa1ead <waliddraa2@gmail.com>
2025-01-23 12:05:00 +0100

    step 150

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -191,8 +191,10 @@ function attack() {
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -206,7 +208,11 @@ function attack() {
   }
 }
 
-function getMonsterAttackValue(level) {}
+function getMonsterAttackValue(level) {
+  const hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
 
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
