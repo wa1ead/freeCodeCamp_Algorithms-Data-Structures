9fcd2a0678a30053e06976319822161d3eefa8b1
wa1ead <waliddraa2@gmail.com>
2025-01-24 16:49:01 +0100

    step 170

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -279,6 +279,13 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
-  text.innerText =
-    "You picked " + guess + ". Here are the random numbers:" + guess;
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  }
 }
