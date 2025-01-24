ba25d5bbd55a3359b6892d59033ecf33edbbda16
wa1ead <waliddraa2@gmail.com>
2025-01-24 16:41:16 +0100

    step 165

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -90,6 +90,12 @@ const locations = [
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
   },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+  },
 ];
 
 // initialize buttons
@@ -268,4 +274,11 @@ function pickEight() {
   pick(8);
 }
 
-function pick(guess) {}
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText =
+    "You picked " + guess + ". Here are the random numbers:" + guess;
+}
