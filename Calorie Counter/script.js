4720da737cb8406025eb5fb65c0eb6708b88ec0b
wa1ead <waliddraa2@gmail.com>
2025-01-27 13:30:19 +0100

    step 25

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -7,5 +7,7 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-  const regex = /\+-/;
+  console.log("original string: ", str);
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
 }
