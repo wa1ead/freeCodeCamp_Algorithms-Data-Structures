0d48b479eb77bb10324fb1acd73e7a0c4d1e5ec3
wa1ead <waliddraa2@gmail.com>
2025-01-27 13:37:12 +0100

    step 35

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -12,5 +12,7 @@ function cleanInputString(str) {
 }
 
 function isInvalidInput(str) {
-  const regex = /e/i;
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
 }
+console.log(isInvalidInput("1e3"));
