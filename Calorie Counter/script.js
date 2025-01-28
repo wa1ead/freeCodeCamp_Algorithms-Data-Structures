3e532765e60c6b7a8d628e66f1ca3a0610d82ec4
wa1ead <waliddraa2@gmail.com>
2025-01-28 09:40:09 +0100

    step 55

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -21,5 +21,18 @@ function addEntry() {
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length;
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.insertAdjacentHTML();
 }
+
+addEntryButton.addEventListener("click", addEntry);
