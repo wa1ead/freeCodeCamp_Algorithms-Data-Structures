85774f040e148a2ce2a233801c7afb258ac973af
wa1ead <waliddraa2@gmail.com>
2025-01-28 09:20:44 +0100

    step 45

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -17,5 +17,9 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = "#" + entryDropdown.value;
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
