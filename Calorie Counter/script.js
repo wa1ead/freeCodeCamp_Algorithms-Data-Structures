e52f1ec2aa4f1a5ed5d2ef9a0d12c184eef9d37c
wa1ead <waliddraa2@gmail.com>
2025-01-29 12:11:22 +0100

    step 75

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -38,9 +38,28 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+
   const breakfastNumberInputs = document.querySelectorAll(
     "#breakfast input[type='number']"
   );
+  const lunchNumberInputs = document.querySelectorAll(
+    "#lunch input[type='number']"
+  );
+  const dinnerNumberInputs = document.querySelectorAll(
+    "#dinner input[type='number']"
+  );
+  const snacksNumberInputs = document.querySelectorAll(
+    "#snacks input[type='number']"
+  );
+  const exerciseNumberInputs = document.querySelectorAll(
+    "#exercise input[type='number']"
+  );
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
 }
 
 function getCaloriesFromInputs(list) {
