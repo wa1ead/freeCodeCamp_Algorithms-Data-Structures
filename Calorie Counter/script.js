bbb2560381ea824b5eaa23cbb21b78745e9f9c98
wa1ead <waliddraa2@gmail.com>
2025-01-29 12:00:08 +0100

    step 65

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -39,7 +39,14 @@ function getCaloriesFromInputs(list) {
   let calories = 0;
 
   for (const item of list) {
-    const currVal = item.value;
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
   }
 }
 
