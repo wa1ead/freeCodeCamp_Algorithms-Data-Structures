b0e15e18ff2c131760dabffeb2469635d68e049b
wa1ead <waliddraa2@gmail.com>
2025-01-30 10:21:46 +0100

    FINISHED

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -101,7 +101,20 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
-function clearForm() {}
+function clearForm() {
+  const inputContainers = Array.from(
+    document.querySelectorAll(".input-container")
+  );
+
+  for (const container of inputContainers) {
+    container.innerHTML = "";
+  }
+
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add("hide");
+}
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
