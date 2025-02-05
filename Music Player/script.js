872c405e4dafbd5de20ef449449c3bcd77c43cfb
wa1ead <waliddraa2@gmail.com>
2025-02-05 13:13:55 +0100

    step 40

--- Music Player/script.js
+++ Music Player/script.js
@@ -85,7 +85,18 @@ let userData = {
   songCurrentTime: 0,
 };
 
-const playSong = (id) => {};
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
