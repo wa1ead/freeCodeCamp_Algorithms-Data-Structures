b8788cce09145a936d328652fc298f6046c10c7b
wa1ead <waliddraa2@gmail.com>
2025-02-09 19:08:02 +0100

    case 1

--- Palindrome Checker Project/script.js
+++ Palindrome Checker Project/script.js
@@ -1,3 +1,15 @@
 const inputText = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
+
+let inputTextValue = inputText.value;
+let resultValue = result.value;
+
+const checkPalindrome = (e) => {
+  e.preventDefault();
+  if (!inputText.value) {
+    alert("Please input a value");
+  }
+};
+
+checkButton.addEventListener("click", checkPalindrome);
