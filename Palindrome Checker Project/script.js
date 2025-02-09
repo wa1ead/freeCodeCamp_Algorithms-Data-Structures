5ae050c96cfa74fd6787a668be85d95cf270736f
wa1ead <waliddraa2@gmail.com>
2025-02-09 19:36:32 +0100

    FINISHED

--- Palindrome Checker Project/script.js
+++ Palindrome Checker Project/script.js
@@ -5,6 +5,7 @@ const result = document.getElementById("result");
 const checkPalindrome = (e) => {
   e.preventDefault();
   let inputTextValue = inputText.value;
+  console.log(inputTextValue);
 
   if (!inputTextValue) {
     alert("Please input a value");
@@ -13,7 +14,21 @@ const checkPalindrome = (e) => {
   if (inputTextValue.length == 1) {
     result.textContent = `${inputTextValue} is a palindrome`;
   }
-  result.textContent = `${inputTextValue} is not a palindrome`;
+
+  const normalisedInput = inputTextValue
+    .toLowerCase()
+    .replace(/[^a-z0-9]/gi, "");
+
+  const palindromeCheck =
+    normalisedInput === normalisedInput.split("").reverse().join("")
+      ? true
+      : false;
+
+  if (palindromeCheck) {
+    result.textContent = `${inputTextValue} is a palindrome`;
+  } else {
+    result.textContent = `${inputTextValue} is not a palindrome`;
+  }
 };
 
 checkButton.addEventListener("click", checkPalindrome);
