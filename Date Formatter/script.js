430c72790ae7e25e1bde304a946da6ac122dddc6
wa1ead <waliddraa2@gmail.com>
2025-02-10 12:08:53 +0100

    step 5

---
+++ Date Formatter/script.js
@@ -0,0 +1,7 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+const day = date.getDate();
+
+const month = date.getMonth() + 1;
