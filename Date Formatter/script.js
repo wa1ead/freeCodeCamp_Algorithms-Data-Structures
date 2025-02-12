dd317d85c5608ae325b6434f42d4a359ba967f2a
wa1ead <waliddraa2@gmail.com>
2025-02-12 12:02:51 +0100

    step 25

--- Date Formatter/script.js
+++ Date Formatter/script.js
@@ -11,11 +11,16 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split("").reverse();
-console.log(exampleSentence);
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
-      currentDateParagraph.textContent = formattedDate;
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = ``;
+      break;
   }
 });
