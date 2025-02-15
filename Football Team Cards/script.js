dcb3a472c53b80d0296275c24c3f803a6190ba01
wa1ead <waliddraa2@gmail.com>
2025-02-15 12:26:43 +0100

    step 30

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -183,7 +183,14 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr.map(
     ({ name, position, number, isCaptain, nickname }) => {
-      return `<div class="player-card"></div>`;
+      return `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname:</p>
+        </div>
+      `;
     }
   );
 };
