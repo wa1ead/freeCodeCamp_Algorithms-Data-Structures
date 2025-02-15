3ea8da6a516459f5435af70ed06a8124eaa24697
wa1ead <waliddraa2@gmail.com>
2025-02-15 12:30:22 +0100

    step 35

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -181,16 +181,20 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
+  playerCards.innerHTML += arr
+    .map(({ name, position, number, isCaptain, nickname }) => {
       return `
         <div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
-          <p>Nickname:</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `;
-    }
-  );
+    })
+    .join("");
 };
+
+playersDropdownList.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
