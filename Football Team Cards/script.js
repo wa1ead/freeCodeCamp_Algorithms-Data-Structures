3ee9c378c5167fc5c4eb57958ade18666bef50ed
wa1ead <waliddraa2@gmail.com>
2025-02-17 12:54:12 +0100

    FINISHED

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -202,5 +202,26 @@ playersDropdownList.addEventListener("change", (e) => {
     case "nickname":
       setPlayerCards(players.filter((player) => player.nickname !== null));
       break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+    case "goalkeeper":
+      setPlayerCards(
+        players.filter((player) => player.position === "goalkeeper")
+      );
+      break;
+
+    default:
+      setPlayerCards();
   }
 });
