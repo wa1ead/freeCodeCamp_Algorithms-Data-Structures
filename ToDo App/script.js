b15e99908a364fbbe29ba552f2b8506ca38e928d
wa1ead <waliddraa2@gmail.com>
2025-02-20 13:51:31 +0100

    step 20

--- ToDo App/script.js
+++ ToDo App/script.js
@@ -32,9 +32,10 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
   const taskObj = {
-    id: titleInput.value.toLowerCase().split(" "),
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
 });
