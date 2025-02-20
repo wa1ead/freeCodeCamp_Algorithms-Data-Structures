6b79678940af507c67c58790022c9699cce7a6d7
wa1ead <waliddraa2@gmail.com>
2025-02-20 13:46:16 +0100

    step 10

--- ToDo App/script.js
+++ ToDo App/script.js
@@ -12,3 +12,22 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
