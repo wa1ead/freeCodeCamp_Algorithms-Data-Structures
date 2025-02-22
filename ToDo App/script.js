0702347fdd8111ca69fb52ea45b12bb06f91da01
wa1ead <waliddraa2@gmail.com>
2025-02-22 12:02:12 +0100

    step 30

--- ToDo App/script.js
+++ ToDo App/script.js
@@ -13,6 +13,8 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {};
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -46,8 +48,14 @@ taskForm.addEventListener("submit", (e) => {
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
         <div class="task" id="${id}">
-      <p><strong>Title:</strong>${title}</p>        
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
         </div>
       `;
   });
+
+  taskForm.classList.toggle("hidden");
 });
