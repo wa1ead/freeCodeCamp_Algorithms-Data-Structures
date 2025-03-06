27d8fe252b8c6fa83133bfa19b56346a9ab38c53
wa1ead <waliddraa2@gmail.com>
2025-03-06 11:36:21 +0000

    step 5

---
+++ Decimal to Binary Converter/script.js
@@ -0,0 +1,11 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const checkUserInput = () => {
+  console.log(numberInput.value);
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", () => {});
