ce41fb4864240e64663bcde21cb5cbb1c2175779
wa1ead <waliddraa2@gmail.com>
2025-03-09 12:38:41 +0000

    step 75

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -3,12 +3,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "";
+  if (input === 0 || input === 1) {
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
+
+const showAnimation = () => {};
 
 const checkUserInput = () => {
   if (
@@ -18,6 +20,9 @@ const checkUserInput = () => {
   ) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
+  }
+
+  if (parseInt(numberInput.value) == 5) {
   }
 
   result.textContent = decimalToBinary(parseInt(numberInput.value));
