6e59362f863022ec128b3d89aef3c7d8545ad7f9
wa1ead <waliddraa2@gmail.com>
2025-03-12 12:05:24 +0000

    FINISHED

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -54,6 +54,10 @@ const showAnimation = () => {
       document.getElementById(obj.inputVal).remove();
     }, obj.removeElDelay);
   });
+
+  setTimeout(() => {
+    result.textContent = decimalToBinary(5);
+  }, 20000);
 };
 
 const checkUserInput = () => {
