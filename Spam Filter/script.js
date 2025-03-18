3515d5e7d2e33a4f5e32fb8b197191e66aa70c11
wa1ead <waliddraa2@gmail.com>
2025-03-18 12:40:18 +0000

    step 10

--- Spam Filter/script.js
+++ Spam Filter/script.js
@@ -2,7 +2,9 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+
+const isSpam = (msg) => helpRegex.test(msg);
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
@@ -13,6 +15,5 @@ checkMessageButton.addEventListener("click", () => {
   result.textContent = isSpam(messageInput.value)
     ? "Oh no! This looks like a spam message."
     : "This message does not seem to contain any spam.";
-
   messageInput.value = "";
 });
