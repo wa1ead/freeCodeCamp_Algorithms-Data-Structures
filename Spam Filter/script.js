b9e9f99146199b84b8db62a0f594c9eaaa9ec2f4
wa1ead <waliddraa2@gmail.com>
2025-03-19 12:46:24 +0000

    step 30

--- Spam Filter/script.js
+++ Spam Filter/script.js
@@ -4,9 +4,10 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
-const freeRegex = /fr[e3][e3] m[o0]n[e3]y/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /st[o0]ck al[e3]rt/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
