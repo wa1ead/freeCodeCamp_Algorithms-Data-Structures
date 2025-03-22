b7e1490a447cf5fdd490663ea84073591704ade8
wa1ead <waliddraa2@gmail.com>
2025-03-22 12:38:27 +0000

    FINISHED

--- Spam Filter/script.js
+++ Spam Filter/script.js
@@ -6,7 +6,7 @@ const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
-const dearRegex = /dear friend/i;
+const dearRegex = /(?:\s|^)d[e3][a@4]r fr[i1|][e3]nd(?:\s|$)/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
