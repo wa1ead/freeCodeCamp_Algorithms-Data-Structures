0c8ded0c7ad3ec660f4a46306b6165875e4e182a
wa1ead <waliddraa2@gmail.com>
2025-03-22 12:45:13 +0000

    step 5

---
+++ Number Sorter/script.js
@@ -0,0 +1,9 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = document.getElementsByClassName("values-dropdown");
+};
+
+sortButton.addEventListener("click", sortInputArray);
