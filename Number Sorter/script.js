60d2da29c41bca19417490475133d5af1b000cfe
wa1ead <waliddraa2@gmail.com>
2025-03-24 13:56:09 +0000

    step 25

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -22,9 +22,17 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
   }
+
+  return array;
 };
+
+const selectionSort = (array) => {};
 
 sortButton.addEventListener("click", sortInputArray);
