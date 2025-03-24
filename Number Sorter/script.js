b77e3140ac7a3c05edd19dc1ee04a8d0d745eabe
wa1ead <waliddraa2@gmail.com>
2025-03-24 13:31:33 +0000

    step 10

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -2,8 +2,9 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
-
-  const inputValues = document.getElementsByClassName("values-dropdown");
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
 };
 
 sortButton.addEventListener("click", sortInputArray);
